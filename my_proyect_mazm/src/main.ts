513aae488e01c157e4c0591f9c749052a7a6a6f9
Moises Zelaya <zelayamoises725@gmail.com>
2026-09-24 21:36:58 -0600

    Agrega PrismaService global y configuracion de Swagger

--- my_proyect_mazm/src/main.ts
+++ my_proyect_mazm/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API my_proyect_mazm')
+    .setDescription('Documentacion de la API del proyecto NestJS')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();
